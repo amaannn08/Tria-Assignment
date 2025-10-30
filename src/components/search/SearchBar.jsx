e3b17a7c4e0849967623671cf70924cc5ba9ac47
linearmusic <amaannn.gupta.08@gmail.com>
2025-10-30 18:51:32 +0530

    fix:Search bar placeholder visiblity bug

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -106,11 +106,12 @@ const SearchBar = () => {
                 ref={inputRef}
                 type="text" 
                 placeholder='Search Contacts' 
-                className={`w-[100%] ml-2 md:text-xl rounded-full text-gray-900 dark:text-gray-100 placeholder-gray-500 dark:placeholder-gray-400 outline-none transition-all duration-300 
+                // className={`w-[100%] ml-2 md:text-xl rounded-full text-gray-900 dark:text-gray-100 placeholder-gray-500 dark:placeholder-gray-400 outline-none transition-all duration-300 
                     
-                    ${inputRef ? "text-gray-900":"text-gray-100"}
+                //     focus:placeholder-transparent
                     
-                    ${selected ? "bg-white dark:bg-gray-700" : "bg-gray-200 dark:bg-gray-700"}`}
+                //     ${selected ? "bg-white dark:bg-gray-700" : "bg-gray-200 dark:bg-gray-700"}`}
+                className={`w-[100%] p-1 ml-2 md:text-xl rounded-full text-gray-900 dark:text-gray-100 placeholder-gray-500 dark:placeholder-gray-400 focus:placeholder-transparent dark:focus:placeholder-transparent outline-none transition-all duration-300 ${selected ? "bg-white dark:bg-gray-700" : "bg-gray-200 dark:bg-gray-700"}`}
                 />
                 <div onClick={clearSearch} className='flex justify-items-center hover:bg-gray-300 dark:hover:bg-gray-600 rounded-full p-1 cursor-pointer transition-colors'>
                     <X className='text-gray-600 dark:text-gray-300 md:w-8 md:h-8'/>
